9e74f3b9ef44dc91276d7489e6df5cc8765125ca
Tristan <tristandeering@hotmail.com>
2018-12-28 19:18:20 +0000

    finished test migrations

--- knex_script.js
+++ knex_script.js
@@ -1,30 +1,25 @@
-const settings = require("./settings");
-const pg = require("pg");
-var knex = require('knex')({
-  client:  'pg',
+const settings = require('./settings');
+const knex = require('knex')({
+  client: 'pg',
   connection: {
-    "user": "development",
-    "password": "development",
-    "database": "test_db",
-    "hostname": "localhost",
-    "port": 5432,
-    "ssl": true
-  },
-  pool: { min: 0, max: 7}
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.host,
+    port: settings.port,
+  }
 });
-const moment = require("moment");
-const input = process.argv.slice(2).toString();
 
-function insertName(first_name, last_name, birthdate) {
-  knex('famous_people')
-  .insert({first_name: `${first_name}`, last_name: `${nachu}`, birthdate:  `${2002-11-16}`}).asCallback(function (err, res) {
-    if (err) {
-      console.log(err)
-    } else {
-    console.log("inserted table")
-    knex.destroy()
-    }
-  })
-}
-insertName(okay, yup, 2016-02-12);
+const newPerson = {
+  'first_name': process.argv[2],
+  'last_name': process.argv[3],
+  'birthdate': process.argv[4]
+};
 
+ const insert = knex('famous_people')
+  .insert(person).asCallback((err, res) => {
+    if (err) return console.error(err);
+    console.log('Row inserted');
+  }).finally(function() {
+    knex.destroy();
+  });
